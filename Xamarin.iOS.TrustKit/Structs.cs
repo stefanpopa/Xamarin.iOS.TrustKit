e55a66fbfdf67ef585c29861ddc66ae26d5d1133
Stefan Popa <popa.stefan.tudor@gmail.com>
2021-02-16 01:22:40 +0200

    - Initial commit.

---
+++ Xamarin.iOS.TrustKit/Structs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ObjCRuntime;
+using Foundation;
+
+namespace Xamarin.iOS.TrustKit
+{
+	[Native]
+	public enum TSKTrustEvaluationResult : long
+	{
+		Success,
+		FailedNoMatchingPin,
+		FailedInvalidCertificateChain,
+		ErrorInvalidParameters,
+		FailedUserDefinedTrustAnchor,
+		ErrorCouldNotGenerateSpkiHash
+	}
+
+	[Native]
+	public enum TSKTrustDecision : long
+	{
+		ShouldAllowConnection,
+		ShouldBlockConnection,
+		DomainNotPinned
+	}
+
+}
+
